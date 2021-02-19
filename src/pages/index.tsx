93b50a81439ec582da753c58ea4ac97a81c9e1ae
wangyidong <wangyidong@sigmayun.com>
2021-02-19 10:53:50 +0800

    feat: 首页填充

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import { history } from 'umi';
+import styles from './index.less'
+import { observer, useLocalObservable } from 'mobx-react'
+
+const storeInitial = {
+    context: 'test',
+    setContext(str: string){
+        this.context = str
+    }
+}
+
+const goHome = () => {
+    history.push('/home');
+}
 
 export default function IndexPage() {
   return (
-    <div>
-      <div onClick={()=>{
-        history.push('/home');
-      }}>
-        home
+    <div className={styles.box}>
+      <div className={styles.content}>
+          <div onClick={goHome} className={styles.title}>王以栋</div>
+          <div className={styles.contentBot}>
+              <div className={styles.left}></div>
+              <div className={styles.right}></div>
+          </div>
+
       </div>
     </div>
   );
